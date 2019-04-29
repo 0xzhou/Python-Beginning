427f16c07c75a3382b9907d393c2b4f19f2d4c9e
Mingy2018 <605329720@qq.com>
2019-04-29 20:46:10 +0200

    1

---
+++ cfish_3.1.py
@@ -0,0 +1,8 @@
+print('--I love Python--')
+temp=input("Guess a number: ")
+guess=int(temp)
+if guess == 8:
+    print("You are right!")
+else:
+    print("Wrong, the right answer is 8")
+print("Game is over.")
