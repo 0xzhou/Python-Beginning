04904ccc1e223b68e50049d125c6469f8360de70
zmy <605329720@qq.com>
2019-07-26 00:22:04 +0200

    1

--- RL-SpinningUp/Simplest_Policy_Gradient.py
+++ RL-SpinningUp/Simplest_Policy_Gradient.py
@@ -25,6 +25,33 @@ def train(env_name='CarPole-v0', hidden_size=[32], lr=1e-2,
     obs_dim=env.observation_space[0]
     n_acts=env.action_space.n
 
+    # 1. Making the Policy Network
     # make core of policy network
     obs_ph=tf.placeholder(shape=(None,obs_dim),dtype=tf.float32)
     logits=mlp(obs_ph,sizes=hidden_size+[n_acts])
+
+    # make action selection op (outputs int actions, sampled from policy)
+    actions=tf.squeeze(tf.multinomial(logits=logits,num_samples=1),axis=1)
+
+    # 2. Making the Loss Function
+    # make loss function whose gradient, for the right data, is policy gradient
+    weights_ph=tf.placeholder(shape=(None,),dtype=tf.float32)
+    act_ph=tf.placeholder(shape=(None,),dtype=tf.int32)
+    action_masks=tf.one_hot(act_ph,n_acts)
+    log_probs=tf.reduce_sum(action_masks*tf.nn.log_softmax(logits),axis=1)
+    loss=-tf.reduce_mean(weights_ph*log_probs)
+
+    #
+
+
+
+
+
+
+
+
+
+
+
+
+
